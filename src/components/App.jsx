9c0d548e691246c084497e844bca867f80ef5741
Andriy-Lik <alan321987@gmail.com>
2023-02-18 23:27:13 +0200

    npm install

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,36 @@
-export const App = () => {
-  return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
-    >
-      React homework template
-    </div>
-  );
-};
+import { Component } from 'react';
+import { nanoid } from 'nanoid';
+import { Layout } from './Layout';
+import { StickerForm } from './StickerForm/StickerForm';
+import { StickerList } from './StickerList/StickerList';
+import initialStickers from '../stickers.json';
+
+export class App extends Component {
+  state = {
+    stickers: initialStickers,
+  };
+
+  addSticker = (img, label) => {
+    this.setState(prevState => ({
+      stickers: [...prevState.stickers, { id: nanoid(), img, label }],
+    }));
+  };
+
+  deleteSticker = stickerId => {
+    this.setState(prevState => ({
+      stickers: prevState.stickers.filter(sticker => sticker.id !== stickerId),
+    }));
+  };
+
+  render() {
+    return (
+      <Layout>
+        <StickerForm onSubmit={this.addSticker} />
+        <StickerList
+          items={this.state.stickers}
+          onDelete={this.deleteSticker}
+        />
+      </Layout>
+    );
+  }
+}
